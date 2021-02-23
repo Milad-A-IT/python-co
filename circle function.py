a416cf8112c2ec012f9f14377b69009e5c32dbf4
Milad A <Milad.a.20it@gmail.com>
2021-02-23 16:57:43 -0600

    commit2

---
+++ circle function.py
@@ -0,0 +1,4 @@
+def weird(x, y, z):  #function definition
+    print((x**x + y**z) // z ) # code block
+
+weird(5,6,7) #function call with passed parameters
--- first_function.py
+++
@@ -1,7 +0,0 @@
-"""def weird_arithmetic(x,y,z)  
-    print(x**x + y**z) // z)  
-
-weird_arithmetic(5,6,7)"""
-def friend(name, greeting) 
-  print(greeting, name + "!")
-friend("john", "Hey")
