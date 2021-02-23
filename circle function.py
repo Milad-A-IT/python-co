55d80f11594a442a9dbcdbb30e230c36b70f91be
Milad A <Milad.a.20it@gmail.com>
2021-02-23 17:02:06 -0600

    commit third

--- circle function.py
+++ circle function.py
@@ -1,4 +1,12 @@
+"""
 def weird(x, y, z):  #function definition
     print((x**x + y**z) // z ) # code block
 
 weird(5,6,7) #function call with passed parameters
+"""
+def weird(x, y, z):  #function definition
+    output = ((x**x + y**z) // z ) # code block
+    return output
+some_number = weird(5,6,7)
+print("some_number:" , some_number)
+ #function call with passed parameters
