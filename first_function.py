c7f3f6ad8a195d8be7c2a765c5046ef7bcc23063
Milad A <Milad.a.20it@gmail.com>
2021-02-23 16:43:57 -0600

    introduction

---
+++ first_function.py
@@ -0,0 +1,4 @@
+def weird_arithmetic(x,y,z)  #function definition line
+    print(x**x + y**z) // z)  #code block
+
+weird_arithmetic(5,6,7)   #function call with pass parameters
--- introduction.py
+++ introduction.py
@@ -4,9 +4,16 @@ name="Milad"
 print ("hello", name + "!")
 print (7/2) #float
 print(7//2)  #integer
-print(27 % 3) """
+print(27 % 3) 
 x=10
 y=3
 print(x+y)
 print(x*y)
 print(x%y)
+print(2**4)"""
+#a=5
+#b=5.0
+#c="5"
+#d=True
+
+print ("hello all" + str("20") + "us" )
