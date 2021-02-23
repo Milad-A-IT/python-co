2a5e261a11fd9b0d1b1df69fb1099e6f2e89e026
Milad A <Milad.a.20it@gmail.com>
2021-02-23 16:50:54 -0600

    commit2

--- first_function.py
+++ first_function.py
@@ -1,4 +1,7 @@
-def weird_arithmetic(x,y,z)  #function definition line
-    print(x**x + y**z) // z)  #code block
+"""def weird_arithmetic(x,y,z)  
+    print(x**x + y**z) // z)  
 
-weird_arithmetic(5,6,7)   #function call with pass parameters
+weird_arithmetic(5,6,7)"""
+def friend(name, greeting) 
+  print(greeting, name + "!")
+friend("john", "Hey")
