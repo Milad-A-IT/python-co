8a9e130a561029de130d0cfab2be986619a208d7
Milad A <Milad.a.20it@gmail.com>
2021-02-24 17:02:41 -0600

    commit

--- lab3.py
+++ lab3.py
@@ -23,7 +23,7 @@
 
 
 
-
+"""
 N = int(input("enter number: "))
 
 if N % 2 != 0:
@@ -34,7 +34,78 @@ else:
     elif N >= 6 and N <= 20:
         print ("Weird")
     elif N > 20:
-        print ("Not Weird")
+        print ("Not Weird")    """
 #++++++++++++++++++++++++++++++++++++++++
+#Q4
+"""(Medium) Write a function that takes an array of integers and places the zeroes in the center of the array.
+ All non-zero integers should keep their relative position. For the purpose of this question, center is defined as floor(length of array / 2).
+ center_zeros([1, 1, 3, 0, 6, 0]) -> [1, 1, 0, 0, 3, 6]"""
+""" from math import floor
 
 
+def center_zeros(array):
+    # write your function here
+    # center means the floor(x / 2) where floor means rounding a float (decimal number) down to the nearest integer
+    # i.e. floor(1) = 1, floor(1.5) = 1, floor(1.75) = 1, floor(2) = 2
+    pass
+
+
+if __name__ == "__main__":
+    # write your debug code here
+    pass"""
+
+"""from question2 import center_zeros
+
+
+def test_1():
+    assert center_zeros([0, 3, 1]) == [3, 0, 1]
+
+
+def test_2():
+    assert center_zeros([1, 1, 3, 0]) == [1, 1, 0, 3]
+
+
+def test_3():
+    assert center_zeros([1, 1, 3, 0, 6, 0]) == [1, 1, 0, 0, 3, 6]
+
+
+def test_4():
+    assert center_zeros([0, 0, 1]) == [0, 0, 1]
+
+
+def test_5():
+    assert center_zeros([]) == []
+
+
+"""
+"""#Q5
+(Warm-up) Write a function that selects the maximum value from an array of integers.
+ Do not use any built-in max functions. select_max([1, 2, 3, 4]) -> 4
+
+ def select_max(array):
+        # write your function here
+    # do NOT use the built-in max() function
+    pass
+
+
+if __name__ == "__main__":
+    # write your debug code here
+    pass"""
+
+#Q6
+
+"""def printValues(l):
+        l = list()
+for i in range(1,21):
+	l.append(i**2)
+	print(l)
+		
+printValues(l)"""
+#===========================================================
+"""#fibonnaci numbers less than 1000
+x,y=0,1
+
+while y<1000:
+    print(y)
+    x,y = y,x+y
+"""
