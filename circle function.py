5641ddae008a5e31343c97f2d9fa33529d07dda6
Milad A <Milad.a.20it@gmail.com>
2021-02-24 09:08:27 -0600

    commit

--- circle function.py
+++ circle function.py
@@ -19,6 +19,7 @@ def fun(radius, height):
     print = (float(pi*radius*radius))
     #return  output
 fun(4, 10)"""
+"""
 #=========================================================
 PI = 3.14
 radius = float(input(' Please Enter the radius of a circle: '))
@@ -26,5 +27,16 @@ area = PI * radius * radius
 circumference = 2 * PI * radius
  
 print(" Area Of a Circle = %.2f" %area)
-print(" Circumference Of a Circle = %.2f" %circumference)
+print(" Circumference Of a Circle = %.2f" %circumference) """
 #=========================================================
+# Python program to find Area of a circle 
+
+def findArea(r): 
+	PI = 3.142
+	return PI * (r*r); 
+
+# Driver method 
+print("Area is %.6f" % findArea(1)); 
+
+
+# This code is contributed by Chinmoy Lenka 
