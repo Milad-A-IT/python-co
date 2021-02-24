a3ade1671687174e047994b72443e3cce66ab4c6
Milad A <Milad.a.20it@gmail.com>
2021-02-24 15:04:12 -0600

    new commit

---
+++ branching_logic.py
@@ -0,0 +1,14 @@
+like_python = True
+like_netflix = False
+
+if like_python:
+    print("i will study now")
+
+elif like_netflix:
+    print("i will relax now")
+
+elif True:
+    print("second elif statement")
+else:
+    print("if everything is fine ,i will print this statement")        
+
