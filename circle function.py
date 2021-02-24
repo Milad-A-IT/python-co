428152459ad87a965b1a8ed556e164751939132d
Milad A <Milad.a.20it@gmail.com>
2021-02-24 08:51:03 -0600

    commit

--- circle function.py
+++ circle function.py
@@ -4,9 +4,27 @@ def weird(x, y, z):  #function definition
 
 weird(5,6,7) #function call with passed parameters
 """
-def weird(x, y, z):  #function definition
+#=========================================================
+"""def weird(x, y, z):  #function definition
     output = ((x**x + y**z) // z ) # code block
     return output
 some_number = weird(5,6,7)
 print("some_number:" , some_number)
- #function call with passed parameters
+ #function call with passed parameters"""
+ #========================================================
+"""
+pi = 3.4 
+def fun(radius, height):
+#output = float(pi*radius*radius))   
+    print = (float(pi*radius*radius))
+    #return  output
+fun(4, 10)"""
+#=========================================================
+PI = 3.14
+radius = float(input(' Please Enter the radius of a circle: '))
+area = PI * radius * radius
+circumference = 2 * PI * radius
+ 
+print(" Area Of a Circle = %.2f" %area)
+print(" Circumference Of a Circle = %.2f" %circumference)
+#=========================================================
